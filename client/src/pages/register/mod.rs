314587e663fcc33a040e1245f87f54acbbe5aa8a
arn-the-long-beard <arn.menant@gmail.com>
2020-08-05 13:37:11 +0200

    feat: added inputs for password completed form and added Msg

--- client/src/pages/register/mod.rs
+++ client/src/pages/register/mod.rs
@@ -2,15 +2,15 @@ use seed::{prelude::*, *};
 use shared::models::user::User;
 pub type Model = User;
 
-struct InputModel {
-    field: String,
-    value: String,
-}
-
 /// Action on register page
 pub enum Msg {
     Register,
-    Verify(String),
+    PasswordChanged(String),
+    UsernameChanged(String),
+    EmailChanged(String),
+    FirstNameChanged(String),
+    LastNameChanged(String),
+
     Clear,
 }
 /// Update on register pages
@@ -18,15 +18,11 @@ pub fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
         Msg::Register => {}
         Msg::Clear => {}
-        Msg::Verify(text) => {
-            log!(model.email);
-            log!("--------------");
-            log!(text);
-
-            model.email = text;
-            log!(model.email);
-            log!("--------------");
-        }
+        Msg::PasswordChanged(text) => log!(text),
+        Msg::UsernameChanged(text) => log!(text),
+        Msg::FirstNameChanged(text) => log!(text),
+        Msg::LastNameChanged(text) => log!(text),
+        Msg::EmailChanged(text) => log!(text),
     }
 }
 
@@ -35,49 +31,74 @@ pub fn view(model: &Model) -> Node<Msg> {
     div![form![
         attrs! {
         At::Action => "/api/register",
-        At::Method=> "/post"
+        At::Method=> "post",
+        At::Target=> "_top"
         },
         label![attrs! { At::For => "username"}, "Username"],
         input![
             id!("username"),
             attrs! {
             At::Required => true,
+            At::Name => model.credentials.username,
+            At::MinLength=> "5",
             At::Name => "username",
+            At::MaxLength=> "15",
             At::Type=> "text"
-                    }
+                    },
+            input_ev(Ev::Input, Msg::UsernameChanged),
         ],
         label![attrs! { At::For => "email"}, "Email"],
         input![
             id!("email"),
             attrs! {
+            At::Required => true,
+            At::Value => model.credentials.email,
+            At::MinLength=> "5",
+            At::MaxLength=> "15"
+            At::Name => "email",
+            At::Type=> "email"
+               },
+            input_ev(Ev::Input, Msg::FirstNameChanged),
+        ],
+        label![attrs! { At::For => "password"}, "Password"],
+        input![
+            id!("password"),
+            attrs! {
                 At::Required => true,
-                At::Value => model.email,
-                At::Name => "email",
-                At::Type=> "email"
+                At::MinLength=> "8",
+                At::MaxLength=> "25"
+                At::Value => model.credentials.password(),
+                At::Name => "password",
+                At::Type=> "password"
             },
-            input_ev(Ev::Input, Msg::Verify),
+            input_ev(Ev::Input, Msg::PasswordChanged),
         ],
         label![attrs! { At::For => "first_name"}, "First Name"],
         input![
             id!("first_name"),
             attrs! {
-                At::Required => true,
-                At::Name => "first_name",
-                At::Type=> "text"
-            }
+            At::Required => true,
+            At::Name => "first_name",
+            At::Type=> "text"
+
+                   },
+            input_ev(Ev::Input, Msg::FirstNameChanged),
         ],
         br![],
         label![attrs! { At::For => "last_name"}, "Last Name"],
         input![
             id!("last_name"),
             attrs! {
-                At::Required => true,
-                At::Name => "last_name",
-                At::Type=> "text"
-            }
+            At::Required => true,
+            At::MaxLength=> "15"
+            At::Name => "last_name",
+            At::Type=> "text"
+                   },
+            input_ev(Ev::Input, Msg::LastNameChanged),
         ],
         br![],
         input![attrs! {
+            At::MaxLength=> "15"
             At::Type=> "submit" ,
             At::Value=> "Register" ,
         }],
